9f59ada17d7623d89bc7aba64373bb5fd75d83f6
Satish <Satish996280@gmail.com>
2021-06-30 16:13:45 +0530

    second commit

--- wright_to_csv.py
+++ wright_to_csv.py
@@ -4,5 +4,5 @@ with open('pytocsv.csv', 'w', newline='') as tf:
     writer = csv.writer(tf)
     writer.writerow(["SN", "Name", "Area"])
     writer.writerow([1, "Ranjan Bharadvaj", "Banglore"])
-    writer.writerow([2, "Harish", "shimla"])
+    writer.writerow([2, "Harish", "shima"])
     writer.writerow([3, "Satish", "delhi"])
